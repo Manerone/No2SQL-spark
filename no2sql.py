e990ec0f010daa6726b4e625d10ee7c53e47d5d2
Matheus Nerone <matheus.agio@hotmail.com>
2017-04-17 20:50:50 -0300

    Docstrings

--- no2sql.py
+++ no2sql.py
@@ -2,15 +2,26 @@ from pyspark import SparkContext, SQLContext
 
 
 class No2SQL:
-    def __init__(self, headers, values, sparkContext=None):
+    """Transforms a csv like to a relational schema"""
+    def __init__(self, headers, values, spark_context=None):
+        """Initialize a new No2SQL object.
+
+        Arguments:
+        headers -- name of each attribute
+        values -- list of instances
+        """
         self.headers = headers
         self.values = values
-        self.context = self._build_spark_context(sparkContext)
+        self.context = self.build_spark_context(spark_context)
         self.sql_context = SQLContext(self.context)
         self.data_frame = self.sql_context.createDataFrame(values, headers)
 
-    def _build_spark_context(self, context):
+    def build_spark_context(self, context):
+        """Creates a new SparkContext if not given."""
         if context is None:
             return SparkContext()
         else:
             return context
+
+    def start(self):
+        pass
