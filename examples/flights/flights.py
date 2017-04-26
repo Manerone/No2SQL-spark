cc52273027a16bed9e9413d8808227d90aea145c
Matheus Nerone <matheus.agio@hotmail.com>
2017-04-26 11:59:34 -0300

    adding examples folder

---
+++ examples/flights/flights.py
@@ -0,0 +1,17 @@
+import os
+current_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(current_path + '/../../.')
+from no2sql import No2SQL
+from pyspark import SparkContext, SQLContext
+
+sc = SparkContext()
+sql_context = SQLContext(sc)
+print 'Reading file'
+df = sql_context.read.format("com.databricks.spark.csv").options(
+    header=True, inferSchema=True
+    ).load(current_path + '/flights.csv.gz')
+print 'Creating No2SQL object'
+no2sql = No2SQL(df, sc)
+print 'No2SQL.start()'
+no2sql.start()
