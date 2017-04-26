9fe1af68d73d02c185427e1845304e4773b4b88a
Matheus Nerone <matheus.agio@hotmail.com>
2017-04-26 11:59:24 -0300

    wip: no2sql

--- no2sql.py
+++ no2sql.py
@@ -1,6 +1,7 @@
-from pyspark import SparkContext, SQLContext
+import os
+current_path = os.path.dirname(os.path.realpath(__file__))
 import sys
-sys.path.append('./no2sql')
+sys.path.append(current_path + '/no2sql')
 from dependency_checker import DependencyChecker
 
 
@@ -8,45 +9,38 @@ class No2SQL:
     """Transforms a csv like to a relational schema.
     This class is an implementation using Apache Spark of the paper:
     Michael DiScala and Daniel J. Abadi. 2016.\
-    Automatic Generation of Normalized Relational Schemas from Nested Key-Value Data
+    Automatic Generation of Normalized Relational Schemas\
+    from Nested Key-Value Data
     """
 
-    def __init__(self, headers, values, spark_context=None, soft_alfa=0.99):
+    def __init__(self, data_frame, spark_context, soft_alfa=0.99):
         """Initialize a new No2SQL object.
 
         Arguments:
         headers -- name of each attribute
         values -- list of instances
         """
-        self.headers = headers
-        self.values = values
         self.soft_alfa = soft_alfa
-        self.context = self._build_spark_context(spark_context)
-        self.sql_context = SQLContext(self.context)
-        self.data_frame = self.sql_context.createDataFrame(values, headers)
-
-    def _build_spark_context(self, context):
-        """Creates a new SparkContext if not given."""
-        if context is None:
-            return SparkContext()
-        else:
-            return context
+        self.context = spark_context
+        self.data_frame = data_frame
 
     def start(self):
         sfd = self._find_soft_func_dependencies
+        print sfd
 
     def _possible_soft_func_dependencies(self):
         sfds = []
-        for i in xrange(len(self.headers)):
-            for j in xrange(i+1, len(self.headers)):
-                sfds.append((self.headers[i], self.headers[j]))
+        headers = self.data_frame.schema.names
+        for i in xrange(len(headers)):
+            for j in xrange(i+1, len(headers)):
+                sfds.append((headers[i], headers[j]))
         return self.context.parallelize(sfds)
 
     def _find_soft_func_dependencies(self):
-        self._possible_soft_func_dependencies.map(
+        return self._possible_soft_func_dependencies().map(
             lambda x: self._is_soft_func_dependency(x[0], x[1])
         )
 
     def _is_soft_func_dependency(self, A, B):
-        if DependencyChecker(self.data_frame, A, B, self.soft_alfa).check()
-            return (A,B)
+        if DependencyChecker(self.data_frame, A, B, self.soft_alfa).check():
+            return (A, B)
